20697e772858cdfd48a31430fc0853be7c4114fd
giulio.ladu <giulio.ladu@matchboxmobile.com>
2016-08-26 13:37:27 +0100

    Added a few comments to code, Added install information to README.md Created package.json

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # AlexaFacebook
 
-Add description of what this project is
+This is a basic tutorial on how to link a Facebook account to an Alexa skill using Amazon Account linking.
 
 ## Install
-How to install the node modules... Should be as simple as 
-npm install
+Make sure you have [Node.js](https://nodejs.org/en/) installed
+
+git clone https://github.com/matchboxmobile/AlexaFacebook.git 
+cd AlexaFacebook
+
+npm install alexa-sdk
+npm install facebook-node
 
 ## How to run it?
-npm start?
+You now need to zip the index.js and the node_modules file. This is what is needed to upload to Amazon when asked to upload code when setting up a Lamba function.
 
-Perhaps a link to the blog post too ?
+NEED LINK TO BLOG POST HERE
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Alexa = require('alexa-sdk');
-var util = require('util');
 var FB = require('facebook-node');
+var util = require('util');
 
+// Messages used for Alexa to tell the user
 var repeatWelcomeMessage = "you should be able to read your feed, and make a post using this skill.";
 
 var welcomeMessage = "Welcome to facebook test skill, " + repeatWelcomeMessage;
@@ -16,31 +17,39 @@ var noAccessToken = "There was a problem getting the correct token for this skil
 
 var accessToken = "";
 
+// Create a new session handler
 var Handler = {
     'NewSession': function () {
 
+        // Access token is pass through from the session information
         accessToken = this.event.session.user.accessToken;
         
+        // If we have an access token we can continue.
         if (accessToken) {
             FB.setAccessToken(accessToken);
-            console.log("HAVE ACCESS TOKEN : " + accessToken);
             this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
         }
         else {
+            // If we dont have an access token, we close down the skill. This should be handled better for a real skill.
             this.emit(':tell', noAccessToken, tryLaterText);
         }
     },
 
+    // Read fb feed handler
     'readFeedIntent': function () {
         var alexa = this;
 
+        // Again check if we have an access token
         if (accessToken) {
+            // Call into FB module and get my feed
             FB.api("/me/feed", function (response) {
                 if (response && !response.error) {
-                    
+                    // If we have data
                     if (response.data) {
                         var output = "";
                         var max = 3;
+
+                        // Take the top three posts and parse them to be read out by Alexa.
                         for (var i = 0; i < response.data.length; i++) {
                             if (i < max) {
                                 output += "Post " + (i + 1) + " " + response.data[i].message + ". ";
@@ -50,7 +59,8 @@ var Handler = {
                     } else {
                         // REPORT PROBLEM WITH PARSING DATA
                     }
-                } else {    
+                } else {
+                    // Handle errors here.
                     console.log(response.error);
                 }
             });
@@ -59,19 +69,24 @@ var Handler = {
         }
     },
 
+    // Write a post to Facebook feed handler.
     'writePostIntent': function () {
 
         var alexa = this;
 
+        // Chack if we have access tokens.
         if (accessToken) {
             FB.api("/me/feed", "POST",
             {
+                // Message to be posted
                 "message": "This is Alexa, I can now access a whole new world of information, good luck!"
             }, function (response) {
                 if (response && !response.error) {
+                    // Alexa output for successful post
                     alexa.emit(':tell', "Post successfull");
                 } else {
                     console.log(response.error);
+                    // Output for Alexa, when there is an error.
                     alexa.emit(':ask', "There was an error posting to your feed, please try again");
                 }
             });
@@ -82,24 +97,27 @@ var Handler = {
     },
 
     'AMAZON.CancelIntent': function () {
-        // Handle cancel intent.
+        // Triggered wheen user asks Alexa top cancel interaction
         this.emit(':tell', stopSkillMessage);
     },
 
     'AMAZON.StopIntent': function () {
-        // Handle stop intent.
+        // Triggered wheen user asks Alexa top stop interaction
         this.emit(':tell', stopSkillMessage);
     },
 
+    // Triggered wheen user asks Alexa for help
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', helpText, helpText);
     },
 
+    // Triggered when no intent matches Alexa request
     'Unhandled': function () {
         this.emit(':ask', helpText, helpText);
     }
 };
 
+// Add handlers.
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(Handler);
